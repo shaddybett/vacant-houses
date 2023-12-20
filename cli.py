1da51502ef96cf41dc8db232c2aed87dcaf4e9a4
Shaddy <shadybett540@gmail.com>
2023-12-20 20:25:09 +0300

    done

--- cli.py
+++ cli.py
@@ -1,21 +1,18 @@
-# operations.py
+# cli.py
 
-from models import session, House
+import argparse
+from operations import get_house_details, list_all_houses
 
-def add_house(location, bedrooms, price):
-    exists = session.query(House).filter_by(location=location).first()
-    if exists:
-        print('Location exists')
+def main():
+    parser = argparse.ArgumentParser(description="House Database CLI")
+    parser.add_argument("--get-details", help="Get details for a specific location")
+
+    args = parser.parse_args()
+
+    if args.get_details:
+        get_house_details(args.get_details)
     else:
-        new_house = House(location=location, bedrooms=bedrooms, price=price)
-        session.add(new_house)
-        session.commit()
+        list_all_houses()
 
-def get_house_details(location):
-    house = session.query(House).filter_by(location=location).first()
-    if house:
-        print(f"Location: {house.location}")
-        print(f"Bedrooms: {house.bedrooms}")
-        print(f"Price: {house.price}")
-    else:
-        print(f"No house found for location: {location}")
+if __name__ == "__main__":
+    main()
--- operations.py
+++ operations.py
@@ -2,15 +2,6 @@
 
 from models import session, House
 
-def add_house(location, bedrooms, price):
-    exists = session.query(House).filter_by(location=location).first()
-    if exists:
-        print('Location exists')
-    else:
-        new_house = House(location=location, bedrooms=bedrooms, price=price)
-        session.add(new_house)
-        session.commit()
-
 def get_house_details(location):
     house = session.query(House).filter_by(location=location).first()
     if house:
@@ -19,3 +10,12 @@ def get_house_details(location):
         print(f"Price: {house.price}")
     else:
         print(f"No house found for location: {location}")
+
+# Optionally, you can add a function to list all houses
+def list_all_houses():
+    houses = session.query(House).all()
+    if houses:
+        for house in houses:
+            print(f"Location: {house.location}, Bedrooms: {house.bedrooms}, Price: {house.price}")
+    else:
+        print("No houses found in the database.")
